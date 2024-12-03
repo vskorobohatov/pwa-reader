969e169d7c785ee62d8058d6af037417cb7f52bb
SnailDog <vskorobohatov@geeksters.io>
2024-12-03 20:07:33 +0200

    Added books get

--- src/pages/Books/index.js
+++ src/pages/Books/index.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./styles.scss";
+import { User } from "services/User";
 
 const Books = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    try {
+      const res = await User.getBooks();
+      setBooks(res?.books || []);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <div className="books-wrapper">
-      
+      {books.map(book => (
+        <div className="book-item" key={book.id}>
+          <div className="name">{book.name}</div>
+        </div>
+      ))}
     </div>
   )
 };
