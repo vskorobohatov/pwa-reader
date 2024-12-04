80d86e4b4ff666c07f8f7f47b5d3fd0d35dd9fca
vskorobohatov <vskorobohatov@geeksters.io>
2024-12-04 14:57:33 +0200

    Fixed errors

--- src/pages/BookReader/index.js
+++ src/pages/BookReader/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ReactReader } from 'react-reader'
 
