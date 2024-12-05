298d8828708e3c273e0e5658ada576add69e31ad
SnailDog <vskorobohatov@geeksters.io>
2024-12-05 15:43:32 +0200

    Updated private router for tests

--- src/PrivateRoute.jsx
+++ src/PrivateRoute.jsx
@@ -1,18 +1,23 @@
 import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { getToken } from "helpers/tokenHelper";
 import { SIGN_IN } from "pathnameVariables";
+import { Button } from "@mui/material";
 
 const PrivateRoute = ({ element }) => {
   const navigate = useNavigate();
+  const location = useLocation();
 
-  useEffect(() => {
-    if (!getToken()) {
-      navigate(SIGN_IN);
-    }
-  }, [getToken(), navigate]);
+  // useEffect(() => {
+  //   if (!getToken()) {
+  //     navigate(SIGN_IN);
+  //   }
+  // }, [getToken(), navigate]);
 
-  return getToken() ? element : <></>;
+  console.log("window location", window.location);
+  console.log("router location", location);
+
+  return getToken() ? element : <><Button onClick={() => navigate(SIGN_IN)}>Sign in</Button ></>;
   // return element
 };
 
