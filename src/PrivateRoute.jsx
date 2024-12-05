c6397d93b87f9120df74e6117216877739241a75
SnailDog <vskorobohatov@geeksters.io>
2024-12-05 16:42:10 +0200

    Fixed htaccess issues

--- src/PrivateRoute.jsx
+++ src/PrivateRoute.jsx
@@ -2,22 +2,18 @@ import React, { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { getToken } from "helpers/tokenHelper";
 import { SIGN_IN } from "pathnameVariables";
-import { Button } from "@mui/material";
 
 const PrivateRoute = ({ element }) => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  // useEffect(() => {
-  //   if (!getToken()) {
-  //     navigate(SIGN_IN);
-  //   }
-  // }, [getToken(), navigate]);
+  useEffect(() => {
+    if (!getToken()) {
+      navigate(SIGN_IN);
+    }
+  }, [getToken(), navigate]);
 
-  console.log("window location", window.location);
-  console.log("router location", location);
-
-  return getToken() ? element : <><Button onClick={() => navigate(SIGN_IN)}>Sign in</Button ></>;
+  return getToken() ? element : <></>;
   // return element
 };
 
